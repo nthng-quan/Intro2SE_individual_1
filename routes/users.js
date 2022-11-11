056f26b427b9a0fc03a2997e09c4a5bb0642e6e6
nthng-quan <hoangquan26102002@gmail.com>
2022-11-11 20:02:30 +0700

    push user with uuid

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,22 @@
 import express from "express";
+import {v4 as uuidv4} from "uuid";
+
 const router = express.Router();
 
-const users = [
-    {
-        "firstName" : "Kanye",
-        "lastName" : "West",
-        "age" : 45
-    },
-    {
-        "firstName" : "Playboi",
-        "lastName" : "Carti",
-        "age" : 26
-    }
-]
+// const users = [
+//     {
+//         "firstName" : "Kanye",
+//         "lastName" : "West",
+//         "age" : 45
+//     },
+//     {
+//         "firstName" : "Playboi",
+//         "lastName" : "Carti",
+//         "age" : 26
+//     }
+// ]
+
+const users = []
 
 router.get('/', (req, res) => {
     console.log(users);
@@ -22,8 +26,9 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const user = req.body
-    users.push(user)
-    
+
+    users.push({...user, id: uuidv4()})
+
     res.send(`User with the name ${req.body.firstName} ${req.body.lastName} added to the database`);
 });
 
