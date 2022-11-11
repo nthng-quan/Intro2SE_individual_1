75c704328e558320ad5541e94a27903f959335dd
nthng-quan <hoangquan26102002@gmail.com>
2022-11-11 20:27:31 +0700

    Update user by patch

--- routes/users.js
+++ routes/users.js
@@ -46,4 +46,17 @@ router.delete('/:id', (req, res) => {
     res.send(`User with id ${id} deleted`);
 });
 
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+    
+    res.send(`User with id ${id} has been updated.`);
+});
+
 export default router
