182f9a36188efd9add1e4204f037147447633af6
nthng-quan <hoangquan26102002@gmail.com>
2022-11-11 20:21:23 +0700

    Delete user by using /delete request

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ const router = express.Router();
 //     }
 // ]
 
-const users = []
+let users = []
 
 router.get('/', (req, res) => {
     console.log(users);
@@ -30,6 +30,20 @@ router.post('/', (req, res) => {
     users.push({...user, id: uuidv4()})
 
     res.send(`User with the name ${req.body.firstName} ${req.body.lastName} added to the database`);
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with id ${id} deleted`);
 });
 
 export default router
