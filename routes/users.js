43d12e7d79ac5f18bace569871ed53847dbbe176
nthng-quan <hoangquan26102002@gmail.com>
2022-11-11 19:16:31 +0700

    Show users in /users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,23 @@
 import express from "express";
 const router = express.Router();
 
+const users = [
+    {
+        "firstName" : "Kanye",
+        "lastName" : "West",
+        "age" : 45
+    },
+    {
+        "firstName" : "Playboi",
+        "lastName" : "Carti",
+        "age" : 26
+    }
+]
+
 router.get('/', (req, res) => {
-    res.send('Users');
+    console.log(users);
+
+    res.send(users);
 });
 
 export default router
