51d18e25fecb9616729577794769e11fe1b863ed
nthng-quan <hoangquan26102002@gmail.com>
2022-11-11 19:58:22 +0700

    users /post

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,11 @@ router.get('/', (req, res) => {
     res.send(users);
 });
 
+router.post('/', (req, res) => {
+    const user = req.body
+    users.push(user)
+    
+    res.send(`User with the name ${req.body.firstName} ${req.body.lastName} added to the database`);
+});
+
 export default router
